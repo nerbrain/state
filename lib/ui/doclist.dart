2d362b9be3cfabadfccacd7aec72f9cb80214fd3
nerbrain <malikmox@gmail.com>
2020-02-19 13:29:33 +0300

    finished doclist.dart file and model.dart

--- lib/ui/doclist.dart
+++ lib/ui/doclist.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:state/util/utils.dart';
 
 import '../model/model.dart';
 import '../util/dbhelper.dart';
@@ -77,9 +78,136 @@ class DocListState extends State<DocList>{
 
     //PAGE96
 
+  void _showResetDialog(){
+
+      showDialog(
+        context: context,
+
+        builder: (BuildContext context){
+          return AlertDialog(
+            title: new Text("Reset"),
+            content: new Text("Do you want to delete all local data?"),
+            actions: <Widget>[
+              FlatButton(
+                child: new Text("Cancle"),
+                onPressed: (){
+                  Navigator.of(context).pop();
+                },
+              ),
+              FlatButton(
+                child: new Text("OK"),
+                onPressed: (){
+                  Future f = _resetLocalData();
+
+                  f.then((result) {
+                    Navigator.of(context).pop();
+                  });
+                },
+              ),
+            ],
+          );
+        },
+      );
+  }
+
+  Future _resetLocalData() async{
+      final dbFuture = dbh.initializedDb();
+      dbFuture.then((result) {
+        final dDocs = dbh.deleteRows(DbHelper.tblDocs);
+        dDocs.then((result) {
+          setState(() {
+            this.docs.clear();
+            this.count = 0;
+          });
+        });
+      });
+  }
+
+
+  void _selectMenu(String value) async {
+      switch (value){
+        case menuReset:
+          _showResetDialog();
+      }
+  }
+
+  ListView docListItems(){
+      return ListView.builder(
+        itemCount: count,
+        itemBuilder: (BuildContext context, int position){
+          String dd = Val.GetExpiryStr(this.docs[position].expiration);
+          String d1 = (dd != "1") ? " days left " : "day left";
+          return Card(
+            color:  Colors.white,
+            elevation: 1.0,
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundColor:
+                (Val.GetExpiryStr(this.docs[position].expiration) != "0")?
+                Colors.blue : Colors.red,
+                child: Text(
+                  this.docs[position].id.toString(),
+                ),
+              ),
+              title: Text(this.docs[position].title),
+              subtitle: Text(
+                Val.GetExpiryStr(this.docs[position].expiration) +d1+
+                  "\nExp: " +DateUtils
+                    .convertToDateFull(this.docs[position].expiration)),
+              onTap: (){
+                navigateToDetail(this.docs[position]);
+              },
+            ),
+          );
+        }
+      );
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    throw UnimplementedError();
+    this.cDate = DateTime.now();
+
+    if(this.docs == null){
+      this.docs = List<Doc>();
+      getData();
+    }
+
+    _checkDate();
+
+    return Scaffold(
+      resizeToAvoidBottomPadding: false,
+      appBar: AppBar(
+        title: Text("DocExpire"),
+        actions: <Widget>[
+          PopupMenuButton(
+            onSelected: _selectMenu,
+            itemBuilder: (BuildContext context){
+              return menuOptions.map((String choice){
+                return PopupMenuItem<String>(
+                  value: choice,
+                  child: Text(choice),
+                );
+              }).toList();
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Scaffold(
+          body: Stack(
+            children: <Widget>[
+              docListItems(),
+            ]),
+          floatingActionButton: FloatingActionButton(
+            onPressed: (){
+              navigateToDetail(Doc.withId(-1, "", "", 1, 1, 1, 1));
+            },
+            tooltip: "Add new doc",
+            child: Icon(Icons.add),
+          ),
+        ),
+      ),
+    );
   }
 }
